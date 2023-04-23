0dbc1aef8a870cbc87d4e6c6ba286f214c53ba66
Addie Reynolds <adelyne@bu.edu>
2023-04-23 15:12:29 -0400

    some updated js that just like half works

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,6 +4,19 @@ const catChoiceIvy = document.getElementById("catChoiceIvy");
 const pipChoices = document.getElementById("pipChoices");
 const ivyChoices = document.getElementById("ivyChoices");
 
+let pipScore = 0;
+let ivyScore = 0;
+
+const startOverBtn = document.getElementById("start-over-btn");
+startOverBtn.addEventListener("click", startOver);
+
+function startOver() {
+  // reset the game to the cat choice
+  window.location.reload(catChoice);
+}
+
+
+
 catChoicePip.addEventListener("change", function() {
     if (catChoicePip.checked) {
         pipChoices.style.display = "block";
@@ -110,6 +123,121 @@ ivyChoices.addEventListener('change', function(event) {
   }
 });
 
+//remove each of Ivy's choices after selection
+
+ivyCouch.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyBed.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyFoodBowl.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyWaterBowl.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyKitchenCounter.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyLitterBox.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyBeanBag.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyPlants.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyBathroomDoor.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyWindow.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+ivyCloset.addEventListener("change", function() {
+  if (this.checked) {
+    ivyChoices.removeChild(this.parentNode);
+  }
+});
+
+//remove each of Ivy's choices after selection
+
+pipCouch.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipBed.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipFoodBowl.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipWaterBowl.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipKitchenCounter.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipLitterBox.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipBeanBag.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipPlants.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipBathroomDoor.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipWindow.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
+pipCloset.addEventListener("change", function() {
+  if (this.checked) {
+    pipChoices.removeChild(this.parentNode);
+  }
+});
 
 
 // get references to the relevant elements
@@ -172,27 +300,44 @@ pipChoices.addEventListener('change', function(event) {
 });
 
 
-const pipBathroomBreakIn = document.getElementById('pipBathroomBreakIn');
-const pipBathroomInfiltrate = document.getElementById('pipBathroomInfiltrate');
+const pipBathroomBreakIn = document.getElementById("pipBathroomBreakIn");
+const pipBathroomInfiltrate = document.getElementById("pipBathroomInfiltrate");
 
 //if cats break into bathroom
 pipBathroomDoor.addEventListener('change', function(event) {
   // get the selected value
-  pipBathroomDoor.style.display = 'none';
   const selectedValue = event.target.value;
+  pipBathroomDoor.style.display = 'none';
   if (selectedValue === pipBathroomBreakIn) {
     pipBathroomInfiltrate.style.display = 'block';
   }
 });
 
-const ivyBathroomInfiltrate = document.getElementById('ivyBathroomInfiltrate');
-const ivyBathroomBreakIn = document.getElementById('ivyBathroomBreakIn');
+
+const ivyBathroomInfiltrate = document.getElementById("ivyBathroomInfiltrate");
+const ivyBathroomBreakIn = document.getElementById("ivyBathroomBreakIn");
 
 ivyBathroomDoor.addEventListener('change', function(event) {
   // get the selected value
-  ivyBathroomDoor.style.display = 'none';
+  
   const selectedValue = event.target.value;
+  ivyBathroomDoor.style.display = 'none';
   if (selectedValue === ivyBathroomBreakIn) {
     ivyBathroomInfiltrate.style.display = 'block';
   }
 });
+
+pipPlaytime.addEventListener("click", pipKeepCycling);
+
+function pipKeepCycling() {
+  // return to the list of choices for Pip, minus the ones already selected this round
+  window.location.reload(pipChoices);
+}
+
+ivyPlaytime.addEventListener("click", ivyKeepCycling);
+
+
+function ivyKeepCycling() {
+  // return to the list of choices for Pip, minus the ones already selected this round
+  window.location.reload(ivyChoices);
+}
