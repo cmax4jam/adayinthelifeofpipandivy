7abe34d0f886be4122afe86738c06ca4ae95fbeb
Addie Reynolds <adelyne@bu.edu>
2023-04-26 15:10:54 -0400

    updated maybe

--- scripts/pipIvyjs.js
+++ scripts/pipIvyjs.js
@@ -1,52 +1,54 @@
 const buildGame = document.getElementById('gameboard');
 const catName = localStorage.getItem('catChoice');
-let Objects = '';
-let catObjects = {};
+
+
+let gameboard = document.getElementById('gameboard');
 
 function buildGame() {
+  let catObjects = {};
   const objects = ['couch', 'bed', 'counter', 'window', 'water bowl', 'food bowl', 'bean bag', 'closet', 'bathroom door', 'litter box', 'plants'];
   objects.forEach(item => {
     let d = document.createElement('div');
     let h3 = document.createElement('h3');
     h3.textContent = item;
-    d.setAttribute(objects);
+    d.setAttribute('Objects', item);
     d.appendChild(h3);
     gameboard.appendChild(d);
-    d.addEventListener('click', (objects) => {
+    d.addEventListener('click', () => {
       catObjects = item;
       updateGame();
     });
   });
 
-  
-    if (catName == 'pip') {
-      catObjects = {
+  let catName = 'pip';
+
+  if (catName == 'pip') {
+    catObjects = {
       couch: ['loaf', 'scratch', 'hide underneath', 'get scared of'],
       bed: ['loaf', 'tear at quilt', 'meow at Ivy'],
       counter: ['loaf', 'jump to window'],
       window: ['watch birds', 'jump on sill'],
-      waterBowl: ['have a little drink', 'get wet'],
-      foodBowl: ['snack', 'throw up food'],
-      beanBag: ['loaf', 'check on hidden hair elastics'],
+      'water bowl': ['have a little drink', 'get wet'],
+      'food bowl': ['snack', 'throw up food'],
+      'bean bag': ['loaf', 'check on hidden hair elastics'],
       closet: ['get fur on clothes', 'loaf on purses', 'play with the belts'],
-      bathroomDoor: ['meow at door', 'break in'],
-      litterBox: ['poop', 'sleep in', 'hide from ivy'],
+      'bathroom door': ['meow at door', 'break in'],
+      'litter box': ['poop', 'sleep in', 'hide from ivy'],
       plants: ['chew and throw up', 'circle suspiciously', 'get scared of']
-      };
-    } else if (catName == 'ivy') {
-      catObjects = {
-        couch: ['loaf', 'scratch', 'make biscuits', 'meow at aimlessly'],
-        bed: ['loaf', 'mess up blankets', 'cuddle with Pip'],
-        counter: ['knock over cups', 'play with stove'],
-        window: ['watch birds', 'scratch screen', 'climb into'],
-        waterBowl: ['have a little drink', 'get water everywhere'],
-        foodBowl: ['snack', 'get food everywhere'],
-        beanBag: ['nap', 'make biscuits', 'jump on'],
-        closet: ['mess up clothes', 'plan world domination', 'play with the belts'],
-        bathroomDoor: ['scratch', 'break in'],
-        litterBox: ['poop', 'drag poop everywhere', 'dig around'],
-        plants: ['sniff', 'knock over', 'climb']
-
-      };
-    }
+    };
+  } else if (catName == 'ivy') {
+    catObjects = {
+      couch: ['loaf', 'scratch', 'make biscuits', 'meow at aimlessly'],
+      bed: ['loaf', 'mess up blankets', 'cuddle with Pip'],
+      counter: ['knock over cups', 'play with stove'],
+      window: ['watch birds', 'scratch screen', 'climb into'],
+      'water bowl': ['have a little drink', 'get water everywhere'],
+      'food bowl': ['snack', 'get food everywhere'],
+      'bean bag': ['nap', 'make biscuits', 'jump on'],
+      closet: ['mess up clothes', 'plan world domination', 'play with the belts'],
+      'bathroom door': ['scratch', 'break in'],
+      'litter box': ['poop', 'drag poop everywhere', 'dig around'],
+      plants: ['sniff', 'knock over', 'climb']
+    };
   }
+}
